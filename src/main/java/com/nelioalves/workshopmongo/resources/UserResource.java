72f3b02ded594a1a792dc7df3dded360379824ec
Matheus <matheus.mnascimento@tivit.com>
2023-02-27 19:15:58 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
@@ -0,0 +1,26 @@
+package com.nelioalves.workshopmongo.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.services.UserService;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+	
+	@Autowired
+	private UserService service;
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+}
